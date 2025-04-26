7b5c2612062047ee6d5eef1ca4779f1e364b3bc6
leir4iks <118904014+leir4iks@users.noreply.github.com>
2025-04-26 12:14:36 +0300

    Update Util.java

--- src/main/java/com/ghostchu/coreprotecttnt/Util.java
+++ src/main/java/com/ghostchu/coreprotecttnt/Util.java
@@ -1,5 +1,6 @@
 package com.ghostchu.coreprotecttnt;
 
+import java.util.Iterator;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.configuration.Configuration;
@@ -8,26 +9,30 @@ import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
 public class Util {
-    public static void broadcastNearPlayers(Location location, String message) {
-        if (message == null || message.isEmpty()) {
-            return; // Do not send empty message
-        }
-        String msg = ChatColor.translateAlternateColorCodes('&', message);
-        for (Entity around : location.getWorld().getNearbyEntities(location, 15, 15, 15, (entity) -> entity instanceof Player)) {
+   public static void broadcastNearPlayers(Location location, String message) {
+      if (message != null && !message.isEmpty()) {
+         String msg = ChatColor.translateAlternateColorCodes('&', message);
+         Iterator var4 = location.getWorld().getNearbyEntities(location, 15.0D, 15.0D, 15.0D, (entity) -> {
+            return entity instanceof Player;
+         }).iterator();
+
+         while(var4.hasNext()) {
+            Entity around = (Entity)var4.next();
             around.sendMessage(msg);
-        }
+         }
 
-    }
+      }
+   }
 
-    public static ConfigurationSection bakeConfigSection(Configuration configuration, String path) {
-        ConfigurationSection section = configuration.getConfigurationSection(path);
-        if (section == null) {
-            // Create default section with default values
-            section = configuration.createSection(path);
-            section.set("enable", true);
-            section.set("disable-unknown", true);
-            section.set("alert", ChatColor.RED + "Failed to read translation, configuration section missing!");
-        }
-        return section;
-    }
+   public static ConfigurationSection bakeConfigSection(Configuration configuration, String path) {
+      ConfigurationSection section = configuration.getConfigurationSection(path);
+      if (section == null) {
+         section = configuration.createSection(path);
+         section.set("enable", true);
+         section.set("disable-unknown", true);
+         section.set("alert", String.valueOf(ChatColor.RED) + "Failed to read translation, configuration section missing!");
+      }
+
+      return section;
+   }
 }
