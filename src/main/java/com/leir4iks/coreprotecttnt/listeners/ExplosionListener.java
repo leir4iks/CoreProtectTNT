404423e7400b02b7183df067f4cf554a3842c12e
leir4iks <leir4iks@gmail.com>
2025-08-01 15:18:48 +0300

    Improve explosion tracking (29)

--- src/main/java/com/leir4iks/coreprotecttnt/listeners/ExplosionListener.java
+++ src/main/java/com/leir4iks/coreprotecttnt/listeners/ExplosionListener.java
@@ -23,15 +23,20 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.util.Vector;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.Objects;
 
 public class ExplosionListener implements Listener {
     private final Main plugin;
+
+    private static final double INTERACTIVE_EXPLOSION_RADIUS = 5.0;
+    private static final double INTERACTIVE_EXPLOSION_ITEM_VELOCITY_MULTIPLIER = 0.8;
+    private static final double MACE_PLAYER_SEARCH_RADIUS = 2.0;
+    private static final double WIND_CHARGE_MACE_SEARCH_RADIUS = 1.5;
 
     public ExplosionListener(Main plugin) {
         this.plugin = plugin;
@@ -56,24 +61,24 @@ public class ExplosionListener implements Listener {
             }
         }
 
-        for (Entity nearbyEntity : center.getWorld().getNearbyEntities(center, 5, 5, 5)) {
-            if (nearbyEntity instanceof Item) {
-                Item item = (Item) nearbyEntity;
-                Vector direction = item.getLocation().toVector().subtract(center.toVector()).normalize();
-                item.setVelocity(item.getVelocity().add(direction.multiply(0.8)));
-            }
+        for (Item item : center.getWorld().getEntitiesByClass(Item.class, center.getBoundingBox().expand(INTERACTIVE_EXPLOSION_RADIUS))) {
+            Vector direction = item.getLocation().toVector().subtract(center.toVector()).normalize();
+            item.setVelocity(item.getVelocity().add(direction.multiply(INTERACTIVE_EXPLOSION_ITEM_VELOCITY_MULTIPLIER)));
         }
+    }
+
+    private boolean isHoldingMace(Player player) {
+        PlayerInventory inventory = player.getInventory();
+        return inventory.getItemInMainHand().getType() == Material.MACE || inventory.getItemInOffHand().getType() == Material.MACE;
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
     public void onBlockExplode(BlockExplodeEvent e) {
         if (e.getBlock().getType() == Material.AIR) {
             Location explosionCenter = e.getBlock().getLocation();
-            for (Entity nearbyEntity : explosionCenter.getWorld().getNearbyEntities(explosionCenter, 2.0, 2.0, 2.0)) {
-                if (nearbyEntity instanceof Player player) {
-                    if (player.getInventory().getItemInMainHand().getType() == Material.MACE ||
-                            player.getInventory().getItemInOffHand().getType() == Material.MACE) {
-
+            for (Player player : explosionCenter.getWorld().getPlayers()) {
+                if (player.getLocation().distanceSquared(explosionCenter) < MACE_PLAYER_SEARCH_RADIUS * MACE_PLAYER_SEARCH_RADIUS) {
+                    if (isHoldingMace(player)) {
                         List<Block> affectedBlocks = new ArrayList<>(e.blockList());
                         e.blockList().clear();
                         handleInteractiveExplosion(affectedBlocks, "#mace-" + player.getName(), explosionCenter);
@@ -90,20 +95,20 @@ public class ExplosionListener implements Listener {
 
         ConfigurationSection section = Util.bakeConfigSection(this.plugin.getConfig(), "block-explosion");
         if (!section.getBoolean("enable", true)) return;
+
         Location location = e.getBlock().getLocation();
         String probablyCauses = this.plugin.getCache().getIfPresent(location);
-        if (probablyCauses == null) {
-            probablyCauses = this.plugin.getCache().getIfPresent(location.getBlock().getLocation());
-        }
-        if (probablyCauses == null) {
-            if (section.getBoolean("disable-unknown", false)) {
-                e.blockList().clear();
-                Util.broadcastNearPlayers(location, section.getString("alert"));
-            }
+
+        if (probablyCauses == null && section.getBoolean("disable-unknown", false)) {
+            e.blockList().clear();
+            Util.broadcastNearPlayers(location, section.getString("alert"));
             return;
         }
-        for (Block block : e.blockList()) {
-            this.plugin.getApi().logRemoval(probablyCauses, block.getLocation(), block.getType(), block.getBlockData());
+
+        if (probablyCauses != null) {
+            for (Block block : e.blockList()) {
+                this.plugin.getApi().logRemoval(probablyCauses, block.getLocation(), block.getType(), block.getBlockData());
+            }
         }
     }
 
@@ -111,17 +116,18 @@ public class ExplosionListener implements Listener {
     public void onEntityExplode(EntityExplodeEvent e) {
         if (e.getEntityType() == EntityType.WIND_CHARGE) {
             String shooterName = this.plugin.getCache().getIfPresent(e.getEntity().getUniqueId());
-            if (shooterName == null) shooterName = "world";
+            if (shooterName == null) {
+                shooterName = "world";
+            }
 
             boolean isMace = false;
             Location explosionCenter = e.getLocation();
-            for (Entity nearbyEntity : explosionCenter.getWorld().getNearbyEntities(explosionCenter, 1.5, 1.5, 1.5)) {
-                if (nearbyEntity instanceof Player player && player.getName().equals(shooterName)) {
-                    if (player.getInventory().getItemInMainHand().getType() == Material.MACE ||
-                            player.getInventory().getItemInOffHand().getType() == Material.MACE) {
-                        isMace = true;
-                        break;
-                    }
+
+            Player shooter = this.plugin.getServer().getPlayerExact(shooterName);
+            if (shooter != null && shooter.getWorld().equals(explosionCenter.getWorld()) &&
+                    shooter.getLocation().distanceSquared(explosionCenter) < WIND_CHARGE_MACE_SEARCH_RADIUS * WIND_CHARGE_MACE_SEARCH_RADIUS) {
+                if (isHoldingMace(shooter)) {
+                    isMace = true;
                 }
             }
 
@@ -145,12 +151,16 @@ public class ExplosionListener implements Listener {
         Entity entity = e.getEntity();
         String track = this.plugin.getCache().getIfPresent(entity.getUniqueId());
 
-        if (track == null) track = this.plugin.getCache().getIfPresent(entity.getLocation());
+        if (track == null) {
+            track = this.plugin.getCache().getIfPresent(entity.getLocation());
+        }
 
         if (track == null) {
-            if (entity instanceof Creeper) {
-                LivingEntity target = ((Creeper) entity).getTarget();
-                if (target != null) track = target.getName();
+            if (entity instanceof Creeper creeper) {
+                LivingEntity target = creeper.getTarget();
+                if (target != null) {
+                    track = target.getName();
+                }
             } else if (entity.getLastDamageCause() instanceof EntityDamageByEntityEvent event) {
                 Entity damager = event.getDamager();
                 String damagerTrack = this.plugin.getCache().getIfPresent(damager.getUniqueId());
@@ -173,7 +183,6 @@ public class ExplosionListener implements Listener {
         String reason = track.startsWith("#") ? track : "#" + e.getEntityType().name().toLowerCase(Locale.ROOT) + "-" + track;
         for (Block block : e.blockList()) {
             this.plugin.getApi().logRemoval(reason, block.getLocation(), block.getType(), block.getBlockData());
-            this.plugin.getCache().put(block.getLocation(), reason);
         }
         this.plugin.getCache().invalidate(entity.getUniqueId());
     }
@@ -183,6 +192,7 @@ public class ExplosionListener implements Listener {
         if (e.getAction() != Action.RIGHT_CLICK_BLOCK) return;
         Block clickedBlock = e.getClickedBlock();
         if (clickedBlock == null) return;
+
         if (clickedBlock.getBlockData() instanceof Bed bed) {
             Location headLocation = (bed.getPart() == Bed.Part.HEAD) ? clickedBlock.getLocation() : clickedBlock.getLocation().add(bed.getFacing().getDirection());
             Location footLocation = (bed.getPart() == Bed.Part.FOOT) ? clickedBlock.getLocation() : clickedBlock.getLocation().subtract(bed.getFacing().getDirection());
@@ -196,7 +206,7 @@ public class ExplosionListener implements Listener {
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onPlayerInteractCreeper(PlayerInteractEntityEvent e) {
-        if (e.getRightClicked() instanceof Creeper && e.getPlayer().getInventory().getItemInMainHand().getType() == Material.FLINT_AND_STEEL) {
+        if (e.getRightClicked().getType() == EntityType.CREEPER && e.getPlayer().getInventory().getItemInMainHand().getType() == Material.FLINT_AND_STEEL) {
             this.plugin.getCache().put(e.getRightClicked().getUniqueId(), "#ignitecreeper-" + e.getPlayer().getName());
         }
     }
